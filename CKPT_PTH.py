2c2a94947249a7871474e2433e19df357fb9da84
Zarif Ben Alam <zarifbenalam@gmail.com>
2026-02-28 09:09:34 +0600

    Add paths for LLAVA CLIP and model

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,4 +1,6 @@
 # Route CLIPs through HuggingFace cache
+LLAVA_CLIP_PATH = 'openai/clip-vit-large-patch14-336'
+LLAVA_MODEL_PATH = 'liuhaotian/llava-v1.5-13b'
 SDXL_CLIP1_PATH = 'openai/clip-vit-large-patch14'
 SDXL_CLIP2_CKPT_PTH = 'laion/CLIP-ViT-bigG-14-laion2B-39B-b160k'
 
