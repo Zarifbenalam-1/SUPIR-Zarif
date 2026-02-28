267d3d42966f054e312439003f8dbc7d0b59377c
Zarif Ben Alam <zarifbenalam@gmail.com>
2026-02-28 09:12:50 +0600

    Initialize CLIP and model paths to None
    Set CLIP and model paths to None for initialization.

--- CKPT_PTH.py
+++ CKPT_PTH.py
@@ -1,9 +1,8 @@
 # Route CLIPs through HuggingFace cache
-LLAVA_CLIP_PATH = 'openai/clip-vit-large-patch14-336'
-LLAVA_MODEL_PATH = 'liuhaotian/llava-v1.5-13b'
-SDXL_CLIP1_PATH = 'openai/clip-vit-large-patch14'
-SDXL_CLIP2_CKPT_PTH = 'laion/CLIP-ViT-bigG-14-laion2B-39B-b160k'
-
+LLAVA_CLIP_PATH = None
+LLAVA_MODEL_PATH = None
+SDXL_CLIP1_PATH = None
+SDXL_CLIP2_CKPT_PTH = None
 # Point directly to where our Docker image will download the safetensors
 SUPIR_ckpt_F_path = '/root/models/SUPIR/SUPIR-v0F_fp16.safetensors'
 SUPIR_ckpt_Q_path = '/root/models/SUPIR/SUPIR-v0Q_fp16.safetensors'
